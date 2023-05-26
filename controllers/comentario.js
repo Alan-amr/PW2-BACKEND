d2ec42a1232b6fe04695f652c8a18f70dfea8230
Alan-amr <72050561+Alan-amr@users.noreply.github.com>
2023-05-26 12:51:14 -0600

    Update comentario.js

--- controllers/comentario.js
+++ controllers/comentario.js
@@ -1,12 +1,33 @@
 const { comentarioModel } = require("../models")
 
-const getItems = async (req,res)=> {
+const getAll = async (req,res)=> {
     const data= await comentarioModel.findAll({})
     res.send({data})
 }
 const getItem = (req,res)=> {}
-const createItem = (req,res)=> {}
-const updateItem = (req,res)=> {}
+const createItem = async (req,res)=> {
+    try {
+        const body = req.body
+        const data = await comentarioModel.create(body)
+        res.data(data);
+    } catch (error) {
+        res.data(error);
+    }
+}
+const updateItem = async (req,res)=> {
+    try {
+        const body = req.body;
+        const data = await comentarioModel.update(body,{
+            where: {
+                id: req.params.id
+            }
+        })
+        res.data(data);
+    } catch (error) {
+        res.data(error);
+        console.log(error);
+    }
+}
 const deleteItem = (req,res)=> {}
 
 
